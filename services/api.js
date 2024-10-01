5b70c48efdf6b9f33d0f4499d61c9eb261ad8656
lucashr <lucas_rdasilva@hotmail.com>
2024-09-30 22:09:17 -0300

    resolução do desafio 5

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://byccjvxmydodkuvkunrn.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5Y2NqdnhteWRvZGt1dmt1bnJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3MzQzMTIsImV4cCI6MjA0MzMxMDMxMn0._mqf_DknPZ1uVFJ5pqFKSkJFg-1nRNfRX2W_7IYGq_s",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5Y2NqdnhteWRvZGt1dmt1bnJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3MzQzMTIsImV4cCI6MjA0MzMxMDMxMn0._mqf_DknPZ1uVFJ5pqFKSkJFg-1nRNfRX2W_7IYGq_s"
     }
 })
